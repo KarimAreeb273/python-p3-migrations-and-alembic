365e08a24c15a1a02aefe7849a33ef09b1eb5ea8
Areeb Karim <areebakarim99@gmail.com>
2023-03-06 12:23:42 -0500

    done

---
+++ lib/migrations/versions/a0117a4cfc3f_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: a0117a4cfc3f
+Revises: 
+Create Date: 2023-03-06 11:56:15.401320
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a0117a4cfc3f'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
